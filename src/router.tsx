cbc07d7c09a1e34ffb6b1d54ff6291da75e49166
Christian da Costa <christiandscosta@gmail.com>
2026-09-03 14:58:15 -0300

    Set basepath for router to '/acolhe-up'

--- src/router.tsx
+++ src/router.tsx
@@ -7,6 +7,7 @@ export const getRouter = () => {
 
   const router = createRouter({
     routeTree,
+    basepath: "/acolhe-up",
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
